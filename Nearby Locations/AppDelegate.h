01fb1e833e5fe660ab4e296d3364b35de34bdda3
Tsif <gtsifrikas@gmail.com>
2015-11-14 15:39:10 +0200

    Initial Commit

---
+++ Nearby Locations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nearby Locations
+//
+//  Created by George Tsifrikas on 14/11/15.
+//  Copyright © 2015 George Tsifrikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nearby Locations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nearby Locations
+//
+//  Created by George Tsifrikas on 14/11/15.
+//  Copyright © 2015 George Tsifrikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
